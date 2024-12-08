5e4b9b7d859cc188a65c38b082baf39e53ac542f
QendresaMorina <qendresa1morina@gmail.com>
2024-12-08 11:50:52 -0800

    Products Finished

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ function validateLoginForm() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Verifikoni që emri i përdoruesit dhe fjalëkalimi janë të plotësuara
     if (username == "" || password == "") {
         alert("Të dyja fushat janë të kërkuara!");
         return false;
     }
 
-    // Shtoni kontrolle të tjera nëse është e nevojshme, siç është validimi i fjalëkalimit
     if (password.length < 6) {
         alert("Fjalëkalimi duhet të ketë të paktën 6 karaktere.");
         return false;
     }
 
-    // Mund të shtoni validim të mëtejshëm këtu për të kontrolluar nëse emri dhe fjalëkalimi janë të sakta me një backend
 
-    // Nëse gjithçka është në rregull, formulari dërgohet
     return true;
 }
-// Function to display the current day
+
 function displayCurrentDay() {
     const daysOfWeek = [
         "Sunday",
@@ -37,6 +33,6 @@ function displayCurrentDay() {
     document.getElementById("current-day").textContent = currentDay;
 }
 
-// Call the function on page load
+
 displayCurrentDay();
 
